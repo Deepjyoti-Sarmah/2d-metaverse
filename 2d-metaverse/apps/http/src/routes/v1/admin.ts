59ee3afcc2566f6939a09aedbd4aac579c782098
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-08 19:46:21 +0530

    adding http requests to the mono repo

---
+++ 2d-metaverse/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+
+adminRouter.post("/spaceId", (req, res) => {
+
+});
+
+adminRouter.put("/element/:elementId", (req, res) => {
+
+});
+
+adminRouter.post("/avatar", (req, res) => {
+
+});
+
+
+adminRouter.post("/map", (req, res) => {
+
+});
