2a353d8a41e5e446736f01269132dcb3bbef141c
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-14 12:28:09 +0530

    adding user and room ws connections

--- 2d-metaverse/apps/ws/src/User.ts
+++ 2d-metaverse/apps/ws/src/User.ts
@@ -2,36 +2,130 @@ import { WebSocket } from "ws";
 import { RoomManager } from "./RoomManager";
 import { OutgoingMessage } from "./types";
 import client from "@repo/db/client";
+import { JWT_PASSWORD } from "./config";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-function generateRandomString (length: number) {
-    const character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    let result = "";
-    for (let i = 0; i < length; i++) {
-        result += character.charAt(Math.floor(Math.random() * character.length));
-    }
-    return result;
+function generateRandomString(length: number) {
+  const character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += character.charAt(Math.floor(Math.random() * character.length));
+  }
+  return result;
 }
 
 export class User {
-    public id: string;
+  public id: string;
+  private spaceId?: string;
+  public userId?: string;
+  private x?: number;
+  private y?: number;
+  private ws: WebSocket;
 
-    constructor(private ws: WebSocket) {
-        this.id = generateRandomString(10);
-    }
+  constructor(ws: WebSocket) {
+    this.id = generateRandomString(10);
+    this.x = 0;
+    this.y = 0;
+    this.ws = ws;
+    this.initHandlers();
+  }
 
-    initHandlers() {
-        this.ws.on("message", (data) => {
-            const parseData = JSON.parse(data.toString());
-            switch (parseData.type) {
-                case "join": {
-                    const spaceId = parseData.payload.spaceId;
-                    RoomManager.getInstance().addUser(spaceId, this);
-                }
+  initHandlers() {
+    this.ws.on("message", async (data) => {
+      const parseData = JSON.parse(data.toString());
+
+      switch (parseData.type) {
+        case "join": {
+
+          const spaceId = parseData.payload.spaceId;
+          const token = parseData.payload.token;
+          const userId = (jwt.verify(token, JWT_PASSWORD) as JwtPayload).userId
+          if (!userId) {
+            this.ws.close();
+            return;
+          }
+          this.userId = userId;
+
+          const space = await client.space.findFirst({
+            where: {
+              id: spaceId
             }
-        });
-    }
+          });
+          if (!space) {
+            this.ws.close()
+            return;
+          }
 
-    send(payload: OutgoingMessage) {
-        this.ws.send(JSON.stringify(payload));
-    }
+          this.spaceId = spaceId;
+          RoomManager.getInstance().addUser(spaceId, this);
+          this.x = Math.floor(Math.random() * space?.width);
+          this.y = Math.floor(Math.random() * space?.height!);
+
+          this.send({
+            type: "space-joined",
+            payload: {
+              spawn: {
+                x: this.x,
+                y: this.y
+              },
+              users: RoomManager.getInstance().rooms.get(spaceId)?.filter(x => x.id !== this.id)?.map((u) => ({ id: u.id })) ?? []
+            }
+          });
+
+          RoomManager.getInstance().broadcast({
+            type: "user-joined",
+            payload: {
+              userId: this.userId,
+              x: this.x,
+              y: this.y
+            }
+          }, this, this.spaceId!);
+          break;
+        }
+        case "move": {
+          const moveX = parseData.payload.x;
+          const moveY = parseData.payload.y;
+
+          const xDisplacement = Math.abs(this.x! - moveX);
+          const yDisplacenent = Math.abs(this.y! - moveY);
+
+          if ((xDisplacement == 1 && yDisplacenent == 0) || (xDisplacement == 0 && yDisplacenent == 1)) {
+            this.x = moveX;
+            this.y = moveY;
+
+            RoomManager.getInstance().broadcast({
+              type: "movement",
+              payload: {
+                x: this.x,
+                y: this.y
+              }
+            }, this, this.spaceId!);
+            return;
+          }
+
+          this.send({
+            type: "movement-rejected",
+            payload: {
+              x: this.x,
+              y: this.y
+            }
+          });
+        }
+      }
+    });
+  }
+
+  destroy() {
+    RoomManager.getInstance().broadcast({
+      type: "user-left",
+      payload: {
+        userId: this.userId
+      }
+    }, this, this.spaceId!);
+    RoomManager.getInstance().removeUser(this, this.spaceId!);
+  }
+
+  send(payload: OutgoingMessage) {
+    this.ws.send(JSON.stringify(payload));
+  }
 }
--- 2d-metaverse/apps/ws/src/index.ts
+++ 2d-metaverse/apps/ws/src/index.ts
@@ -1,13 +1,14 @@
 import { WebSocketServer } from 'ws';
+import { User } from './User';
 
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+  let user = new User(ws);
 
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
-    });
+  ws.on('error', console.error);
 
-    ws.send('something');
+  ws.on('close', () => {
+    user?.destroy();
+  });
 });
